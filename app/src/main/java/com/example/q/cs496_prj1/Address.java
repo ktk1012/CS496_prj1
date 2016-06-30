45c6b89bd98e99f1e6a0561798a5fa3444cf3faf
ktk1012 <kimtkyeom@kaist.ac.kr>
2016-07-01 02:38:51 +0900

    Implement expandable address item and remove entry with long press Snakbar.

--- app/src/main/java/com/example/q/cs496_prj1/Address.java
+++ app/src/main/java/com/example/q/cs496_prj1/Address.java
@@ -6,6 +6,9 @@ package com.example.q.cs496_prj1;
 public class Address {
     public String name;
     public String addr;
+    public String email;
+    public String gender;
+    private boolean is_expand;
 
     String get_name() {
         return this.name;
@@ -15,9 +18,31 @@ public class Address {
         return this.addr;
     }
 
-    Address (String name, String addr) {
-        this.name = name;
-        this.addr = addr;
+    String get_email() {
+        return this.email;
     }
 
+    String get_gender() {
+        return this.gender;
+    }
+
+    Boolean get_is_expand() {
+        return this.is_expand;
+    }
+
+    void set_is_expanded(boolean flag) {
+        this.is_expand = flag;
+    }
+
+    void flip_is_expanded() {
+        this.is_expand = !this.is_expand;
+    }
+
+    Address (String name, String addr, String email, String gender) {
+        this.name = name;
+        this.addr = addr;
+        this.email = email;
+        this.gender = gender;
+        this.is_expand = false;
+    }
 }
--- app/src/main/java/com/example/q/cs496_prj1/CreateAddrActivity.java
+++ app/src/main/java/com/example/q/cs496_prj1/CreateAddrActivity.java
@@ -5,15 +5,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
-import android.text.Editable;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.FileOutputStream;
 
 public class CreateAddrActivity extends AppCompatActivity {
 
@@ -22,30 +21,29 @@ public class CreateAddrActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_addr);
         Button bt1 = (Button) findViewById(R.id.AddBtn1);
-        EditText ed1 = (EditText) findViewById(R.id.add_num);
+        EditText ed1 = (EditText) findViewById(R.id.edit_num);
         ed1.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
 
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                String name = ((EditText) findViewById(R.id.add_name)).getText().toString();
-                String num = ((EditText) findViewById(R.id.add_num)).getText().toString();
-                if (name.length() == 0 || num.length() == 0){
+                String name = ((EditText) findViewById(R.id.edit_name)).getText().toString();
+                String num = ((EditText) findViewById(R.id.edit_num)).getText().toString();
+                Spinner genderSpinner = (Spinner) findViewById(R.id.edit_gender);
+                String gender = genderSpinner.getSelectedItem().toString();
+                String email = ((EditText) findViewById(R.id.edit_email)).getText().toString();
+                if (name.length() == 0 || num.length() == 0 ||
+                        gender.length() == 0 || email.length() == 0) {
                     Snackbar.make(v, "Please fill the name or phone number", Snackbar.LENGTH_SHORT)
                             .setAction("Action", null).show();
                     return;
-                }
-                JSONObject jObj = new JSONObject();
-                try {
-                    jObj.put("name", name);
-                    jObj.put("addr", num);
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
                 Bundle extra = new Bundle();
                 Intent intent = new Intent();
                 extra.putString("name", name);
                 extra.putString("num", num);
+                extra.putString("email", email);
+                extra.putString("gender", gender);
                 intent.putExtras(extra);
                 /* Write to file */
                 setResult (1, intent);
